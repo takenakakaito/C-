e7fe164042e522c381b4ed60c5725a1e1f9eff63
XPS\ktc <ktc25a32a0010@edu.kyoto-tech.ac.jp>
2026-05-09 14:49:19 +0900

    5/9

---
+++ 課題/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+#include <conio.h> 
+
+int main()
+{
+	int money;
+	printf("金額を入力してください");
+	scanf_s(" % d",&money);
+	bool finish = false;
+	while (!finish)
+	{
+		if (money == 0)
+		{
+			printf("両替できません");
+			finish = true;
+		}
+		int Chenge1 = money / 500;
+		int botti1 = money % 500;
+		printf("");
+	}
+
+
+	return 0;
+}
